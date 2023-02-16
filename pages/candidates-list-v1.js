dd18368cb80f3f0e8837ae3a7b7feb341e6dba26
unknown <ifratellidelweb@gmail.com>
2023-02-16 11:46:33 +0100

    For Deploy - 1

--- pages/candidates-list-v1.js
+++ pages/candidates-list-v1.js
@@ -1,21 +1,14 @@
 import dynamic from "next/dynamic";
 import Seo from "../components/common/Seo";
 import CandidatesListV1 from "../components/candidates-listing-pages/candidates-list-v1";
-export const getStaticProps = async () => {
-    const res = await fetch('http://localhost:3000/api/candidates');
-    const data = await res.json();
- 
-    return{props:{dataCL : data}}
-}
-const index = ({dataCL}) => {
 
-    console.log(dataCL)
-    return (
-        <>
-            <Seo pageTitle="Candidates List V1" />
-            <CandidatesListV1 dataCL = {dataCL} />
-        </>
-    );
+const index = () => {
+  return (
+    <>
+      <Seo pageTitle="Candidates List V1" />
+      <CandidatesListV1 />
+    </>
+  );
 };
 
 export default dynamic(() => Promise.resolve(index), { ssr: false });
--- pages/home-9.js
+++ pages/home-9.js
@@ -1,17 +1,12 @@
 import dynamic from "next/dynamic";
 import Seo from "../components/common/Seo";
 import Home9 from "../components/home-9";
-export const getStaticProps = async () => {
-  const res = await fetch('http://localhost:3000/api/candidates');
-  const data = await res.json();
 
-  return{props:{dataCL : data}}
-}
-const index = ({dataCL}) => {
+const index = () => {
   return (
     <>
       <Seo pageTitle="Home-9" />
-      <Home9 dataCL = {dataCL} />
+      <Home9 />
     </>
   );
 };
