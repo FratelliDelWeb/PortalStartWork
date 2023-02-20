a86bee2fba9c9af2edcd6474ab6ac57276c22d67
Antonio Maretta <115124146+FratelliDelWeb@users.noreply.github.com>
2023-02-20 19:22:42 +0100

    PUSH TO  DEPLOY

--- pages/area-privata-candidates/applied-jobs/index.js
+++ pages/area-privata-candidates/applied-jobs/index.js
@@ -1,6 +1,6 @@
 import dynamic from "next/dynamic";
 import Seo from "../../../components/common/Seo";
-import AppliedJobs from "../../../components/dashboard-pages/candidates-dashboard/applied-jobs";
+import AppliedJobs from "../../../components/dashboard-pages/area-privata-candidates/applied-jobs"
 
 const index = () => {
   return (
