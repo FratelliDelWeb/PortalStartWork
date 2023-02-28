182f88dcbab281564f0737dc357df56116ab9b19
unknown <ifratellidelweb@gmail.com>
2023-02-28 16:51:22 +0100

    middleware refactor

--- middleware.ts
+++ middleware.ts
@@ -4,20 +4,17 @@ import { getToken } from "next-auth/jwt";
 export default async function middleware(req: NextRequest) {
   const {pathname} = req.nextUrl;
   console.log('Middleware - Enter to path : ' + pathname)
-  if(pathname.startsWith("/area-privata")){
-    console.log('Middleware - Enter in Private Route')
-    const token = await getToken({req});
-    
 
-    console.log("Token Middleware => ", token)
-  
-    // redirect user without access to login
+    if(pathname.startsWith("/area-privata/")){
+      console.log('Middleware - Enter in Private Route')
+      const token = await getToken({req});
+      console.log("Token Middleware => ", token)
+      // redirect user without access to login
     if (token?.token && Date.now() / 1000 < token?.exp) {
       req.nextUrl.pathname = "/login";
       return NextResponse.redirect(req.nextUrl);
     }
-    if(pathname.startsWith("/area-privata/")){
-      console.log('Middleware - Enter in Private Route - Dashboard')
+      console.log('Middleware - Enter in Private Route - Admin, Basic')
       switch(token?.role){
         case "admin":
         case "Basic":
@@ -29,6 +26,14 @@ export default async function middleware(req: NextRequest) {
       }
     }
     if(pathname.startsWith("/area-privata-candidates/")){
+      console.log('Middleware - Enter in Private Route')
+      const token = await getToken({req});
+      console.log("Token Middleware => ", token)
+      // redirect user without access to login
+    if (token?.token && Date.now() / 1000 < token?.exp) {
+      req.nextUrl.pathname = "/login";
+      return NextResponse.redirect(req.nextUrl);
+    }
       console.log('Middleware - Enter in Private Route - Candidates')
       if (token?.role !== "candidate") {
           req.nextUrl.pathname = "/login";
@@ -36,8 +41,6 @@ export default async function middleware(req: NextRequest) {
       }
     }
     NextResponse.next();
-  }
-  NextResponse.next();
 }
 
   export const config = { matcher : "/((?!.*\\.).*)" } ; 
