c77c011b9e6a43b9cd971b0dd53f202167ee771c
unknown <ifratellidelweb@gmail.com>
2023-03-10 18:52:17 +0100

    fix invocie

--- components/dashboard-pages/area-privata-candidates/my-resume/components/invoice/index.jsx
+++ components/dashboard-pages/area-privata-candidates/my-resume/components/invoice/index.jsx
@@ -79,21 +79,16 @@ const index = ({candidateView}) => {
           <div className="invoice-wrap">
             <div className="invoice-content">
               <div className="logo-box">
-                <div className="logo">
-                  <Link href="/">
-                    <img src="images/logo.svg" alt="logo" />
-                  </Link>
-                  <h5>  {candidateView.name}  {candidateView.surname} </h5>
-                </div>
-                <div className="invoice-id">
-                  N.S<span>{candidateView._id}</span>
-                </div>
+               
               </div>
               {/* End logobox */}
 
            {/*    <InfoBox /> */}
+           <h4> ESPERIENZA LAVORATIVA </h4>
               {/* End infobox */}
-
+              <h4> ISTRUZIONE E FORMAZIONE </h4>
+              <h4>  ULTERIORI INFORMAZIONI </h4>
+             
               <div className="table-outer">
                {/*  <TableInvoice /> */}
               </div>
@@ -103,6 +98,12 @@ const index = ({candidateView}) => {
 
             <div className="invoice-footer">
               <ul className="bottom-links">
+              <li className="logo">
+    
+                  <img src="/images/logo.svg" alt="brand"></img>
+              
+                 
+                </li>
                 <li>
                   <a
                     href="https://themeforest.net/user/ib-themes"
@@ -110,14 +111,14 @@ const index = ({candidateView}) => {
                     rel="noopener noreferrer"
                     className="link"
                   >
-                    www.ibthemespro.com
+                    www.start-work.it
                   </a>
                 </li>
                 <li>
-                  <a href="invoice@superio.com">invoice@superio.com</a>
+                  <a href="invoice@superio.com">info@start-work.com</a>
                 </li>
                 <li>
-                  <a href="tel:123123456">(123) 123-456</a>
+                  <a href="tel:351 990 7274">351 990 7274</a>
                 </li>
               </ul>
             </div>
